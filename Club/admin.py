54fe85d78d453c1345887115464f8ef334e0b5cf
Tay <tyjingyi@gmail.com>
2022-02-04 17:33:42 -0800

    added superuser in admin

--- Club/admin.py
+++ Club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meeting, Meeting_minutes, Resource, Event
 
 # Register your models here.
+admin.site.register(Meeting)
+admin.site.register(Meeting_minutes)
+admin.site.register(Resource)
+admin.site.register(Event)
