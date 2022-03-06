2d4e20105a2b92779fcb0163b820f92acfda8998
Tay <tyjingyi@gmail.com>
2022-03-06 00:56:56 -0800

    A5: added href to base index and list of resources to resource.html

--- Club/views.py
+++ Club/views.py
@@ -2,8 +2,12 @@
 # https://docs.djangoproject.com/en/4.0/topics/http/shortcuts/
 # https://docs.djangoproject.com/en/4.0/topics/http/views/
 
+# Add a view that returns all the resources (A5)
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from Club.models import Resource
+
 
 # Create your views here.
 # A3 -> Create a view called "index." It doesn't need to do anything at this point 
@@ -11,6 +15,12 @@ from django.http import HttpResponse
 def index(request):
 
     return render(request,'index.html')
+
+# A5 -> A view that returns all the resources
+def view_resource(request):
+    resource_list = Resource.objects.all()
+    
+    return render(request, 'resources.html',{'resource_list':resource_list})
 
 
 
